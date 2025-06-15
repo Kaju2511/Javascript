e138751613badbfca38907e1ac2c9f6ab85e5d10
Kaju2511 <152508121+Kaju2511@users.noreply.github.com>
2025-06-15 07:16:27 +0000

    datatypes

---
+++ .devcontainer/01_basic/01_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+
+//alert(3 + 3)//we are using nodejs, not browser
+
+let name = "kajal"
+let age = 20
+let isLoggedIn = false
+//number => 2 to pwer 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standlone vale
+//undefined =>
+//symbol => unique
+
+//object
---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "kajalt2018@gmail.com"
+var accountPassword = "12345"
+//ye tarika sahi nhi h 
+accountCity = "Kolkata"
+
+
+accountEmail = "kajalt2g@gmail.com"
+accountCity = "pune"
+accountPassword = "2548252"
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
+
+/* Prefer not to use var
+because of issue in block scope and functional scope
+*/
